0129b9920d177eb278418a9d85dc1136700deaf7
Kishlay Kr. Singh <88032644+zorg-kishlay@users.noreply.github.com>
2022-02-08 15:43:57 +0530

    Time: 320 ms (67.63%), Space: 0B (100.00%) - LeetHub

--- 182-duplicate-emails/182-duplicate-emails.sql
+++ 182-duplicate-emails/182-duplicate-emails.sql
@@ -1,5 +1,13 @@
 # Write your MySQL query statement below
 
-SELECT email as Email from 
-(Select email, count(email) as c from Person group by email) as temp
-where c>1
+# SELECT email as Email from 
+# (Select email, count(email) as c from Person group by email) as temp
+# where c>1
+
+# Or using Having clause
+
+
+Select email from Person 
+group by email
+Having count(email) > 1
+
