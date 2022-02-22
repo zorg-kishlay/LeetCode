23ee2a1330ffcbd579a46bfb9dae5efbc5dc8bbb
Kishlay Kr. Singh <88032644+zorg-kishlay@users.noreply.github.com>
2022-02-22 18:09:31 +0530

    Time: 540 ms (67.51%), Space: 16.9 MB (6.46%) - LeetHub

---
+++ 743-network-delay-time/743-network-delay-time.py
@@ -0,0 +1,36 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # O(ElogV)
+        # worst case E = V2 where we will pushing each edge to heap
+        # so heap operation would be logV2 and sicne we do it for all edges
+        # O(ElongV2)== O(2ElogV)== O(ElogV)
+
+        edges = collections.defaultdict(list)
+
+        # creating adjacency list
+        for vertex1, vertex2, weight in times:
+            print(vertex1, vertex2, weight)
+            edges[vertex1].append((weight, vertex2))
+
+        # creating a min heap with value 0 and starting vertex k
+
+        min_heap = [(0, k)]
+        visited = set()
+        result = 0
+
+        while min_heap:
+            weight, vertex = heapq.heappop(min_heap)
+            if vertex in visited:
+                continue
+            # to avoid loops
+            visited.add(vertex)
+            result = max(result, weight)
+
+            # so basically check for neighbours
+            for weight2, neighbour in edges[vertex]:
+                if neighbour not in visited:
+                    # pushing weight2+weight because we want to keep track of
+                    # total distance to reach the node
+                    heapq.heappush(min_heap, (weight2 + weight, neighbour))
+
+        return result if len(visited) == n else -1
