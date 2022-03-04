33bfbca1dc4e5372672ff11e2391e20bbca9f53f
Kishlay Kr. Singh <88032644+zorg-kishlay@users.noreply.github.com>
2022-03-04 17:33:04 +0530

    Time: 146 ms (82.14%), Space: 15.2 MB (47.75%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,20 +1,39 @@
 class Solution:
     def jump(self, array) -> int:
+        
+        
+        # O(N):- greedy
+        
+        result = 0
+        # for the window we are checking
+        left = right = 0
+        
+        while right < len(array)-1:
+            farthest = 0
+            for i in range(left,right+1):
+                farthest = max(farthest, i+array[i])
+            left = right+1
+            right = farthest # if we go past range even then we are safe because of line 11
+            result += 1
+        
+        return result
+        
+        
 
-        if len(array) == 1:
-            return 0
+#         if len(array) == 1:
+#             return 0
 
-        jumps = 0
-        max_reach = array[0]
-        steps = array[0]
-        for i in range(1, len(array) - 1):
-            max_reach = max(max_reach, array[i] + i)
-            steps -= 1
-            if steps == 0:
-                jumps += 1
-                steps = max_reach - i
+#         jumps = 0
+#         max_reach = array[0]
+#         steps = array[0]
+#         for i in range(1, len(array) - 1):
+#             max_reach = max(max_reach, array[i] + i)
+#             steps -= 1
+#             if steps == 0:
+#                 jumps += 1
+#                 steps = max_reach - i
 
-        return jumps + 1
+#         return jumps + 1
             
       #  jumps = [float("inf") for i in range(len(array))]
 #         jumps[0] = 0
