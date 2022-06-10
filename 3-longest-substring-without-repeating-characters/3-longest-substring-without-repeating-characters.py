fa04742d463f26f399cf64c4bb81ba63d228923d
Kishlay Kr. Singh <88032644+zorg-kishlay@users.noreply.github.com>
2022-06-10 15:10:26 +0530

    Time: 111 ms (38.70%), Space: 13.9 MB (93.04%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,23 +1,19 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         
-        # O(N) space and time
-        
         seen = ''
         max_len = 0
-        
         for ch in s:
-            # ch not in the seen add it to it
+            
             if ch not in seen:
                 seen+=ch
             
             else:
-                # if ch is in seen we need to move past it
-                # that is we take seen after the first occurence of ch and then append the newely appeared
-                # character to it 
                 seen = seen[seen.index(ch)+1:]+ch
             
-            max_len=max(max_len,len(seen))
+            max_len = max(max_len,len(seen))
+        
         
         return max_len
-                
+            
+        
