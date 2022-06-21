aaccf5655230d6040b04e5d2357059938b74258f
Kishlay Kr. Singh <88032644+zorg-kishlay@users.noreply.github.com>
2022-06-21 15:34:21 +0530

    Time: 146 ms (53.13%), Space: 17.4 MB (29.77%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,47 +1,42 @@
 class Solution:
-    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> \
-            List[int]:
-
-        pre_req = {c: [] for c in range(numCourses)}
-
-        # adding pre req for each course
-        for course, prereq in prerequisites:
-            pre_req[course].append(prereq)
-
-        # course has 3 states
-        # visited : course visited and added to output
-        # visiting: course not added to output but added to cycle
-        # unvisited: course not added to output not added to cycle
-        output = []
-        # 2 sets for tracking visiting and visited
-        visited, cycle = set(), set()
-
-        def dfs(crs):
-
-            # if course in cycle means that our graph has a cycle and
-            # we need to terminate
-            if crs in cycle:
-                return False
-
-            # if we have already visited the course no need to revisit
-            if crs in visited:
-                return True
-
-            cycle.add(crs)
-
-            for pre in pre_req[crs]:
-
-                # cycle detected
-                if not dfs(pre):
-                    return False
-            # since the course's path is done
-            # we remove it from cycle set
-            cycle.remove(crs)
-            visited.add(crs)
-            output.append(crs)
-            return True
-
+    
+    def getAdjMap(self, numCourses, prerequisites):
+        
+        adjMap = {i:[] for i in range(numCourses)}
+        
+        for dest,src in prerequisites:
+            adjMap[src].append(dest)
+        
+        return adjMap
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+        adjMap = self.getAdjMap(numCourses, prerequisites)
+        visited = [-1]*numCourses
+        arrival = [-1]*numCourses
+        dept = [-1]*numCourses
+        #parent = [-1]*numCourses
+        result = []
+        timestamp= 0
+        def dfs(node):
+            visited[node]=1
+            arrival[node]=timestamp+1
+            
+            for ngb in adjMap[node]:
+                if visited[ngb]==-1:
+                    if dfs(ngb):
+                        return True
+                
+                else:
+                    if dept[ngb] == -1: # implies we are at ancestor node
+                        return True
+            
+            dept[node]=timestamp+1
+            result.append(node)
+            return False
+        
         for i in range(numCourses):
-            if not dfs(i):
-                return []
-        return output
+            if visited[i]==-1:
+                if dfs(i):
+                    return []
+        result.reverse()
+        return result
