6e895d76f6600b3153f7543e846bf3e62a3d4bdb
Kishlay Kr. Singh <88032644+zorg-kishlay@users.noreply.github.com>
2022-06-23 16:13:19 +0530

    Time: 75 ms (27.00%), Space: 13.8 MB (96.92%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -6,14 +6,20 @@ class Solution:
         rows = len(grid)
         cols = len(grid[0])
         
-        table = [[0 for _ in range(cols+1)] for _ in range(rows+1)]
-        table[0][1]=1 # first reachable node i.e starting node
+        table = [[0 for _ in range(cols)] for _ in range(rows)]
+        table[0][0]=1 if not grid[0][0] else 1 # If possible to reach starting node
         
-        for i in range(1,rows+1):
-            for j in range(1,cols+1):
-                if grid[i-1][j-1]!=1:
-                    table[i][j]=table[i-1][j]+table[i][j-1]
+        for i in range(rows):
+            for j in range(cols):
+                if not grid[i][j]:
+                    
+                    if i-1>=0:
+                        table[i][j]+=table[i-1][j]
+                    
+                    if j-1>=0:
+                        table[i][j]+=table[i][j-1]
         
         
         return table[-1][-1]
         
+        # Time and Space :- O(mn)
