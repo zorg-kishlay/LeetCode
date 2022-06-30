a3bdcf15c0a336d5ca28e9675cc70d92b4b649df
Kishlay Kr. Singh <88032644+zorg-kishlay@users.noreply.github.com>
2022-06-30 17:37:25 +0530

    Time: 2096 ms (50.65%), Space: 14.3 MB (52.56%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,21 +1,17 @@
-# Time:- O(amount * len(coins))
-# space :- O(amount)
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        coinMin = [float("inf") for i in range(amount+1)]
         
-        # for 0 there will be no coins that can make 0 
-        coinMin[0] = 0
-        for coin in coins:
-            for am in range(len(coinMin)):
-                # amount can only be formed by coins having value less than or equal to it
-                if coin<=am:
-                    # adding 1 because we will be adding this coin to make up to the amount
-                    #e.x for making 5 we will add 2+3 so we are chosing 3 so 1 is added
-                    coinMin[am]=min(coinMin[am],1+coinMin[am-coin])
-        
-        
-        return coinMin[amount] if coinMin[amount]!= float("inf") else -1
-                    
+        dp = [-1]* (amount+1)
+        dp[0]=0
+        for i in range(1,amount+1):
+            minValue = float("inf")
+            for coin in coins:
+                if i-coin>=0:
+                    minValue = min(minValue,dp[i-coin])
             
+            dp[i]=minValue+1 # selecting current coin
+        
+        
+        return dp[amount] if dp[amount]!=float("inf") else -1
+                
         
