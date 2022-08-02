6f416efb0ed0cc6ff6959337193b52569763be5d
Kishlay Kr. Singh <88032644+zorg-kishlay@users.noreply.github.com>
2022-08-02 15:18:35 +0530

    Time: 42 ms (71.20%), Space: 13.9 MB (73.95%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,22 +1,21 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
-        # total no of paths = C(m+n-2,m-1)
-        table= [[0 for _ in range(n)] for _ in range(m)]
+        dp = [[0 for _ in range(n)] for _ in range(m)]
         
-        # for 0th row and cloumn we have only 1 way to reach them either right or bottom
+        # first for first row and column there is only 1 possibility
+        
         for i in range(m):
-            table[i][0]=1
-            
+            dp[i][0]=1
+        
         for i in range(n):
-            table[0][i]=1
+            dp[0][i]=1
         
         
         for i in range(1,m):
             for j in range(1,n):
-                table[i][j]=table[i-1][j]+table[i][j-1]
-        
-        return table[m-1][n-1]
+                dp[i][j]=dp[i-1][j]+dp[i][j-1]
         
         
-        # Time and Space:- O(mn) can improve space by only storing 2 rows or cols based on m and n values
+        return dp[m-1][n-1]
+        
