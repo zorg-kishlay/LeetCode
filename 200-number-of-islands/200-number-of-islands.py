6f7f0e4316f4a12e26214267f2a2d0ac03312a1f
Kishlay Kr. Singh <88032644+zorg-kishlay@users.noreply.github.com>
2022-08-29 14:54:03 +0530

    Time: 438 ms (64.58%), Space: 16.5 MB (65.84%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,30 +1,30 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        # looks like an alteration of the connected components problem
+        # we will do dfs and edit the existing matrix
+        # this problem is similar to number of connected components in graph
         
-        islands = 0
         
         rows = len(grid)
         cols = len(grid[0])
+        noOfIslands = 0
         
-        def modify_grid(i,j):
-            if i< 0 or j< 0 or i>= rows or j>= cols or grid[i][j]=='0':
+        def changeNegh(row,col):
+            if row<0 or row>=rows or col<0 or col>=cols or grid[row][col]!="1":
                 return
+            grid[row][col]="X"
+            for x,y in (1,0),(0,1),(-1,0),(0,-1):
+                changeNegh(row+x,col+y)
             
-            grid[i][j]='0'
-            for diffx,diffy in [(-1,0),(1,0),(0,-1),(0,1)]:
-                modify_grid(i+diffx,j+diffy)
         
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j]=='1':
-                    
-                    # basically when we find a new component we inc island count
-                    islands+=1
-                    modify_grid(i,j)
-                    
-                    # there could have been 2 approaches 1 we maintain a visted array
-                    # here we will just modify the neighbours of our rows to 0 so that don't count as
-                    # separate islands
-        return islands
+        
+        
+        for row in range(rows):
+            for col in range(cols):
+                
+                if grid[row][col]=="1":
+                    noOfIslands+=1
+                    changeNegh(row,col)
+        
+        
+        return noOfIslands
